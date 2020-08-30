5bab40f8277811c56ccc0b75fd7b5a70cc29289d
unknown <silentcommentator1216@gmail.com>
2020-08-30 19:00:44 +0800

    Merged folder 1-3

--- 1_basic_threading/Driver.java
+++ 1_basic_threading/Driver.java
@@ -3,6 +3,8 @@
 public class Driver {
 
     public static void main(String[] args) {
+        // Since CustomThread extends the Thread Class,
+        // we can directly instatiate it to a Thread Object
         Thread ct2 = new CustomThread(2000);
         Thread ct1 = new CustomThread(1000);
 
@@ -17,7 +19,50 @@ public class Driver {
             e.printStackTrace();
         }
 
-        System.out.println("Done");
+        // Instead of the Class of Object is being instatiate,
+        // We can instantiate a new Thread with the CustomThread
+        // since the CustomThread is a Runnable Object
+        Thread t1 = new Thread(new CustomThread("Hello in 2s", 2000));
+        Thread t2 = new Thread(new CustomThread("Hello in 0.5s", 500));
+        
+        try {
+            t1.start();
+            t2.start();
+
+            t1.join();
+            t2.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // We can use a lambda expression to declare the run() function
+        Thread lt1 = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+                System.out.println("Hello in 1s");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread lt2 = new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+                System.out.println("Hello in 2s");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        try {
+            lt1.start();
+            lt2.start();
+
+            lt.join();
+            lt2.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- 2_using_runnable/run.sh
+++
@@ -1,3 +0,0 @@
-javac *.java
-
-java Driver
